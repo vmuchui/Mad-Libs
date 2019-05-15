05e92cdf4644e3c5fb5e4cfc4f277dd705c29ad3
vmuchui <kaigongimuchui@gmail.com>
2019-05-15 23:51:03 +0300

    adds get method for the form template.

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/",(request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model,layout);
+    }, new VelocityTemplateEngine());
+    // get("/story",(request, response) -> {
+    //   Map<String, Object> model = new HashMap<String, Object>();
+    //   model.put("template","templates/story.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+  }
+}
